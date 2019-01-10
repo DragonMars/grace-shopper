fa805aecbe78ade6c337648d372442b4dfe969ba
Laura <laura.waters22@gmail.com>
2019-01-10 15:49:26 -0500

    updating order route

--- client/routes.js
+++ client/routes.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
-import {Login, Signup, UserHome} from './components'
+import {Login, Signup, UserHome, ShippingAddressForm} from './components'
 import {me} from './store'
 import OrderConfirmation from './components/order-confirmation'
 
@@ -25,6 +25,7 @@ class Routes extends Component {
         }
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
+        <Route path="/shipping-address-form" component={ShippingAddressForm} />
         {isLoggedIn && (
           <Switch>
             {/* Routes placed here are only available after logging in */}
