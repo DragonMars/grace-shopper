a3aa904ea6225adc6378c5b8d27b515b246a6119
Laura <laura.waters22@gmail.com>
2019-01-10 18:39:12 -0500

    shipping address redux store

---
+++ server/api/shipping-address.js
@@ -0,0 +1,11 @@
+// const router = require('express').Router()
+// const { Order, ShippingAddress, LineItem } = require('../db/models')
+// module.exports = router
+
+// const newAddress = await ShippingAddress.create({
+//   name: req.body.shippingAddress.name,
+//   steetAddress: req.body.shippingAddress.streetAddress,
+//   city: req.body.shippingAddress.city,
+//   state: req.body.shippingAddress.state,
+//   zipcode: req.body.shippingAddress.zipcode
+// })
