5200c4918eab4799c17966269ec0902c7d2d256a
jentcain <jentcain@gmail.com>
2019-01-10 14:28:37 -0500

    feat: /success path

--- client/routes.js
+++ client/routes.js
@@ -21,7 +21,7 @@ class Routes extends Component {
       <Switch>
         {
           /* Routes placed here are available to all visitors */
-          <Route path="/" component={OrderConfirmation} />
+          <Route path="/success" component={OrderConfirmation} />
         }
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
