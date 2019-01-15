026c2dbd3b7752ba6fbf38426885e1b72dc3dafe
Laura <laura.waters22@gmail.com>
2019-01-15 14:46:40 -0500

    fixed single product view to pull single product data from db

--- client/components/single-product.js
+++ client/components/single-product.js
@@ -3,11 +3,15 @@ import {connect} from 'react-redux'
 import {setOrUpdateItem} from '../store'
 import {Card, Image, Container, Button} from 'semantic-ui-react'
 import {withRouter} from 'react-router-dom'
+import axios from 'axios'
 
 class SingleProduct extends Component {
   constructor(props) {
     super(props)
     this.addToCart = this.addToCart.bind(this)
+    this.state = {
+      singleProduct: {}
+    }
   }
 
   addToCart(productId) {
@@ -15,48 +19,48 @@ class SingleProduct extends Component {
     setOrUpdateCart({productId})
   }
 
+  async componentDidMount() {
+    const {data} = await axios.get(
+      `/api/products/${this.props.match.params.productId}`
+    )
+    console.log('single product', data)
+    this.setState({singleProduct: data})
+  }
+
   render() {
-    const {products} = this.props
-    const productId = Number(this.props.match.params.productId)
-    const [singleProduct] = products.filter(product => {
-      console.log('product.id', product.id)
-      return product.id === productId
-    })
-    console.log('singleProduct', singleProduct)
+    const {singleProduct} = this.state
     return (
       <Container>
-        <Card id="single-product-information">
-          <Image
-            id="single-product-image"
-            alt={singleProduct.altText}
-            src={singleProduct.imageUrl}
-          />
-          <Card.Content id="single-product-details">
-            <Card.Header id="single-product-name">
-              {singleProduct.name}
-            </Card.Header>
-            <Card.Description id="single-product-description">
-              {singleProduct.description}
-            </Card.Description>
-            <Card.Content extra id="single-product-price">
-              Price: ${singleProduct.price / 100}
+        {singleProduct.id !== undefined && (
+          <Card id="single-product-information">
+            <Image
+              id="single-product-image"
+              alt={singleProduct.altText}
+              src={singleProduct.imageUrl}
+            />
+            <Card.Content id="single-product-details">
+              <Card.Header id="single-product-name">
+                {singleProduct.name}
+              </Card.Header>
+              <Card.Description id="single-product-description">
+                {singleProduct.description}
+              </Card.Description>
+              <Card.Content extra id="single-product-price">
+                Price: ${singleProduct.price / 100}
+              </Card.Content>
             </Card.Content>
-          </Card.Content>
-          <Button
-            type="submit"
-            onClick={event => this.addToCart(singleProduct.id, event)}
-            id="add-to-cart"
-          >
-            Add to Cart
-          </Button>
-        </Card>
+            <Button
+              type="submit"
+              onClick={event => this.addToCart(singleProduct.id, event)}
+              id="add-to-cart"
+            >
+              Add to Cart
+            </Button>
+          </Card>
+        )}
       </Container>
     )
   }
-}
-
-const mapStateToProps = state => {
-  return {products: state.product}
 }
 
 const mapDispatchToProps = dispatch => ({
@@ -64,6 +68,4 @@ const mapDispatchToProps = dispatch => ({
     dispatch(setOrUpdateItem({productId}))
   }
 })
-export default withRouter(
-  connect(mapStateToProps, mapDispatchToProps)(SingleProduct)
-)
+export default withRouter(connect(null, mapDispatchToProps)(SingleProduct))
