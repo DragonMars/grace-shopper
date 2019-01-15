4d0cade0a650b5105f3cea705053cae62e38d941
Laura <laura.waters22@gmail.com>
2019-01-15 12:44:31 -0500

    getting ready to GO CRAZY

--- client/components/user-home.js
+++ client/components/user-home.js
@@ -1,19 +1,52 @@
-import React from 'react'
+import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
+import axios from 'axios'
 
 /**
  * COMPONENT
  */
-export const UserHome = props => {
-  const {email} = props
+class UserHome extends Component {
+  constructor() {
+    super()
+    this.state = {
+      orderHistory: []
+    }
+  }
 
-  return (
-    <div>
-      <h3>Welcome, {email}</h3>
-    </div>
-  )
+  async componentDidMount() {
+    const orderHistory = await axios.get('/api/orders')
+    this.setState({orderHistory})
+  }
+
+  render() {
+    const {email} = this.props
+    const {orderHistory} = this.state
+    return (
+      <div>
+        <h3>Welcome, {email}</h3>
+        {orderHistory.length &&
+          orderHistory.map(order => (
+            <div key={order.id}>
+              <p>{order.lineItems}</p>
+              <p>{order.shippingAddress}</p>
+            </div>
+          ))}
+      </div>
+    )
+  }
 }
+
+//TO DELETE
+// export const UserHome = props => {
+//   const {email} = props
+
+//   return (
+//     <div>
+//       <h3>Welcome, {email}</h3>
+//     </div>
+//   )
+// }
 
 /**
  * CONTAINER
