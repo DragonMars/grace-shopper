62fd3497a12146566bcf8a42197f474c92d96cc9
jentcain <jentcain@gmail.com>
2019-01-15 22:26:08 -0500

    feat: login/signup styling

--- client/components/auth-form.js
+++ client/components/auth-form.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import PropTypes from 'prop-types'
-import {Form, Container, Message} from 'semantic-ui-react'
-import {Link} from 'react-router-dom'
+import {Form, Container, Message, Header, Segment} from 'semantic-ui-react'
 import {auth} from '../store'
 
 /**
@@ -13,20 +12,43 @@ const AuthForm = props => {
 
   return (
     <Container>
-      <Form onSubmit={handleSubmit} name={name}>
-        <Form.Input required label="Email" name="email" placeholder="Email" />
-        <Form.Input
-          required
-          label="Password"
-          name="password"
-          type="password"
-          placeholder="password"
-        />
-        <Form.Button>{displayName}</Form.Button>
-        {error &&
-          error.response && <Message negative> {error.response.data} </Message>}
-        <Link to="/auth/google">{displayName} with Google</Link>
-      </Form>
+      <Message info>
+        {displayName === 'Log in' ? (
+          <Segment>
+            <Header size="large">log in</Header>
+          </Segment>
+        ) : (
+          <Segment>
+            <Header size="large">create an account</Header>
+          </Segment>
+        )}
+        <Segment>
+          <Form onSubmit={handleSubmit} name={name}>
+            <Form.Group widths="equal">
+              <Form.Input
+                fluid
+                required
+                label="email"
+                name="email"
+                placeholder="email"
+              />
+              <Form.Input
+                fluid
+                required
+                label="password"
+                name="password"
+                type="password"
+                placeholder="password"
+              />
+            </Form.Group>
+            <Form.Button color="teal">{displayName}</Form.Button>
+            {error &&
+              error.response && (
+                <Message negative> {error.response.data} </Message>
+              )}
+          </Form>
+        </Segment>
+      </Message>
     </Container>
   )
 }
@@ -41,7 +63,7 @@ const AuthForm = props => {
 const mapLogin = state => {
   return {
     name: 'login',
-    displayName: 'Login',
+    displayName: 'Log in',
     error: state.user.error
   }
 }
@@ -49,7 +71,7 @@ const mapLogin = state => {
 const mapSignup = state => {
   return {
     name: 'signup',
-    displayName: 'Sign Up',
+    displayName: 'Sign up',
     error: state.user.error
   }
 }
